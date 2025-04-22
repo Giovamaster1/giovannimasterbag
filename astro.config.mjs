1989ec2af7a7e133698863599ede571f9d9b6ba3
Giovanni <giovannicarvajal@gmail.com>
2025-04-22 13:57:38 -0500

    fix: astro.config listo para deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import 'dotenv/config';
+import dotenv from 'dotenv/config';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
   server: {
     host: true
   },
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [react()]
 });
