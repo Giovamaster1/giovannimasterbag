27fa310afa59cf51bdb2eabcd2d2cb490f05ab4d
Giovanni <giovannicarvajal@gmail.com>
2025-04-22 13:53:57 -0500

    fix: astro.config listo para Railway

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   server: {
     host: true
   },
-
   integrations: [react()]
 });
